12c49e95d2657c089dc8880f9154724c55a1f0ef
amanhussain196 <79570218+amanhussain196@users.noreply.github.com>
2026-03-19 17:14:43 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
-const { chromium } = require('playwright');
+const { chromium } = require('playwright-extra');
+const stealth = require('puppeteer-extra-plugin-stealth')();
+chromium.use(stealth);
 const cors = require('cors');
 const path = require('path');
 
